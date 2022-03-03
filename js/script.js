93c587aa3f9d9ece7b78872e2ddec9499ff7ebcf
Grzegorz_Szwed <greg152295@gmail.com>
2022-03-03 19:35:15 +0100

    added two function to change flags according what is in currency field

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ const currencyTwo = document.querySelector("#currency-two");
 const amountTwo = document.querySelector(".appBody__sectionRight-amountTwo");
 const swapBtn = document.querySelector(".appBody__swap");
 const rateInfo = document.querySelector(".rateInfo");
+const imgOne = document.querySelector(".imgOne");
+const imgTwo = document.querySelector(".imgTwo");
 
 const calculate = () => {
   fetch(
@@ -20,11 +22,69 @@ const calculate = () => {
       )} ${currency2}`;
 
       amountTwo.value = (amountOne.value * rate).toFixed(3);
+      changeFlagsOne();
+      changeFlagsTwo();
     });
+};
+
+const changeFlagsOne = () => {
+  const currentText = currencyOne.options[currencyOne.selectedIndex].text;
+  switch (currentText) {
+    case "PLN":
+      imgOne.setAttribute("src", "./img/plFlag.png");
+      break;
+    case "USD":
+      imgOne.setAttribute("src", "./img/usdFlag.png");
+      break;
+    case "GBP":
+      imgOne.setAttribute("src", "./img/gbpFlag.png");
+      break;
+    case "EUR":
+      imgOne.setAttribute("src", "./img/euroFlag.png");
+      break;
+    case "RUB":
+      imgOne.setAttribute("src", "./img/rusFlag.png");
+      break;
+    case "CNY":
+      imgOne.setAttribute("src", "./img/chnFlag.png");
+      break;
+  }
+};
+
+const changeFlagsTwo = () => {
+  const currentText = currencyTwo.options[currencyTwo.selectedIndex].text;
+  switch (currentText) {
+    case "PLN":
+      imgTwo.setAttribute("src", "./img/plFlag.png");
+      break;
+    case "USD":
+      imgTwo.setAttribute("src", "./img/usdFlag.png");
+      break;
+    case "GBP":
+      imgTwo.setAttribute("src", "./img/gbpFlag.png");
+      break;
+    case "EUR":
+      imgTwo.setAttribute("src", "./img/euroFlag.png");
+      break;
+    case "RUB":
+      imgTwo.setAttribute("src", "./img/rusFlag.png");
+      break;
+    case "CNY":
+      imgTwo.setAttribute("src", "./img/chnFlag.png");
+      break;
+  }
+};
+
+const swap = () => {
+  const oldValue = currencyOne.value;
+  currencyOne.value = currencyTwo.value;
+  currencyTwo.value = oldValue;
+
+  calculate();
 };
 
 currencyOne.addEventListener("change", calculate);
 currencyTwo.addEventListener("change", calculate);
 amountOne.addEventListener("input", calculate);
-
+swapBtn.addEventListener("click", swap);
 calculate();
